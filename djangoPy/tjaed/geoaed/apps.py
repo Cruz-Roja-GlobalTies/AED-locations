e5e5fdeaca899e5aae12454e61ef327f16d21659
jhartzler <johnm.hartzler@gmail.com>
2016-08-04 14:29:15 -0700

    Continued tutorial, fixed bugs arising from following 1.9 tutorial instead of 1.8

--- djangoPy/tjaed/geoaed/apps.py
+++ djangoPy/tjaed/geoaed/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class AedtjConfig(AppConfig):
+class GeoAEDConfig(AppConfig):
     name = 'geoaed'
--- djangoPy/tjaed/geoaed/models.py
+++ djangoPy/tjaed/geoaed/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class AED(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    detail = models.CharField(max_length = 200)
